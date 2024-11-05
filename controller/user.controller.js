73512174a7495bf12ac83933d41502730f6c5039
LakshmiNarasimhan273 <bachelorofphotography@gmail.com>
2024-11-05 12:19:41 +0530

    Login and register api done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,12 +1,15 @@
 import user from "../model/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const registerApi = async (req, res) => {
-    try{
-        const {username, email, password, address} = req.body;
+    try {
+        const { username, email, password, address } = req.body;
         const existingUser = await user.findOne({ email });
 
-        if(existingUser){
+        if (existingUser) {
             return res.status(400).json({ message: "User email already exist" });
         }
         const salt = 10;
@@ -19,9 +22,28 @@ const registerApi = async (req, res) => {
         });
         await userData.save();
         res.status(201).json({ message: `Welcome, ${userData.username}! you seccessfully registered in our portal` });
-    }catch(err){
+    } catch (err) {
         res.status(500).json({ message: `Internal Server error,${err.message}` });
     }
 }
 
-export default {registerApi};
+const loginApi = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const registeredUser = await user.findOne({ email });
+        if (!registeredUser) {
+            return res.status(404).json({ message: "User mail doesn't exist" });
+        };
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+        const token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Authentication failed" });
+    }
+};
+
+export default { registerApi, loginApi };
